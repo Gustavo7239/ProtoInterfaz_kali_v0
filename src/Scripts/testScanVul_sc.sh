163e43506bc4d6595c677767c4e9eb5ab3d29a88
GustavoCruz <>
2024-07-22 22:00:51 +0200

    v2

--- src/Scripts/testScanVul_sc.sh
+++ src/Scripts/testScanVul_sc.sh
@@ -1,1 +1,7 @@
-sudo arp-scan --localnet -q -I eth0 | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v -e "^$my_ip$" -e '\.254$' -e '\.1$' 
+#!/bin/bash                                                                           
+
+# Obtener la IP del ordenador
+my_ip=$(hostname -I | awk '{print $1}')
+
+# Ejecutar arp-scan y filtrar las IPs no deseadas
+sudo arp-scan --localnet -q -I eth0 | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v -e "^$my_ip$" -e '\.254$' -e '\.1$
