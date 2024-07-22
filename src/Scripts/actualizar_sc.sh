582de6c05bcb94abfcd63813409c7fdca1a421e4
GustavoCruz <>
2024-07-22 05:44:04 +0200

    fixing fails in update service

--- src/Scripts/actualizar_sc.sh
+++ src/Scripts/actualizar_sc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Definir la ruta raíz del proyecto
-PROJECT_ROOT="../.."
+PROJECT_ROOT="$(dirname "$(dirname "$(dirname "$0")")")"
 
 # Definir la ruta del archivo origen y destino
 SOURCE_FILE="$PROJECT_ROOT/src/Tools/UpdateProto"
-DEST_FILE="$PROJECT_ROOT/UpdateProto"
+DEST_FILE="$PROJECT_ROOT/../UpdateProto"
 
 # Comprobar si el archivo de destino existe
 if [ -f "$DEST_FILE" ]; then
-    echo "El archivo UpdateProto ya existe en la raíz del proyecto. No se realiza ninguna acción."
+    echo "El archivo UpdateProto ya existe en el directorio padre del proyecto. No se realiza ninguna acción."
 else
     # Copiar el archivo desde la ubicación de origen a la ubicación de destino
     cp "$SOURCE_FILE" "$DEST_FILE"
-    echo "El archivo UpdateProto se ha copiado a la raíz del proyecto."
+    echo "El archivo UpdateProto se ha copiado al directorio padre del proyecto."
 fi
