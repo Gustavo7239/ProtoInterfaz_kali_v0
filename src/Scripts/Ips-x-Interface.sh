bf917e5fcf715291cdcbfcc44255190d7696be40
GustavoCruz <>
2024-07-24 02:33:47 +0200

    update ips x interface

--- src/Scripts/Ips-x-Interface.sh
+++ src/Scripts/Ips-x-Interface.sh
@@ -6,5 +6,5 @@ interface=${1:-eth0}
 # Obtener la IP del ordenador
 my_ip=$(hostname -I | awk '{print $1}')
 
-# Ejecutar arp-scan y filtrar las IPs no deseadas
-sudo arp-scan --localnet -q -I $interface | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v -e "^$my_ip$" -e '\.254$' -e '\.1$'
+# Ejecutar arp-scan, filtrar las IPs no deseadas, ordenar y eliminar duplicados
+sudo arp-scan --localnet -q -I $interface | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v -e "^$my_ip$" -e '\.254$' -e '\.1$' | sort | uniq
