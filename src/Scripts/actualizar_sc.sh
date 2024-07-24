a4d85c4c90c06fead9ce5e41c14cb24c3d825025
GustavoCruz <>
2024-07-24 02:03:22 +0200

    update v5

--- src/Scripts/actualizar_sc.sh
+++ src/Scripts/actualizar_sc.sh
@@ -7,6 +7,9 @@ PROJECT_ROOT="$(dirname "$(dirname "$(dirname "$0")")")"
 SOURCE_FILE="$PROJECT_ROOT/src/Tools/UpdateProto"
 DEST_FILE="$PROJECT_ROOT/../UpdateProto"
 
+echo "La ruta de origen es: $SOURCE_FILE"
+echo "La ruta de destino es: $DEST_FILE"
+
 # Comprobar si el archivo de destino existe
 if [ -f "$DEST_FILE" ]; then
     echo "El archivo UpdateProto ya existe en el directorio padre del proyecto. No se realiza ninguna acción."
--- src/Tools/UpdateProto.sh
+++ src/Tools/UpdateProto.sh
@@ -1,5 +1,52 @@
-sudo rm -r ProtoInterfaz_kali_v0/
+#!/bin/bash
 
-git clone https://github.com/Gustavo7239/ProtoInterfaz_kali_v0.githttps://github.com/Gustavo7239/ProtoInterfaz_kali_v0.git
+ejecutar() {
+    echo "Ejecutando el programa..."
+    python ProtoInterfaz_kali_v0/index.py
+}
 
-python ProtoInterfaz_kali_v0/index.py
+actualizar() {
+    echo "Actualizando el programa..."
+    sudo rm -r ProtoInterfaz_kali_v0/
+    git clone https://github.com/Gustavo7239/ProtoInterfaz_kali_v0.githttps://github.com/Gustavo7239/ProtoInterfaz_kali_v0.git
+    python ProtoInterfaz_kali_v0/index.py
+}
+
+instalarRecursos() {
+    echo "Comprobando e instalando los recursos necesarios..."
+
+    if ! command -v nmap &> /dev/null; then
+        echo "Instalando nmap..."
+        sudo apt-get install -y nmap
+    else
+        echo "nmap ya está instalado."
+    fi
+
+    if ! command -v arp-scan &> /dev/null; then
+        echo "Instalando arp-scan..."
+        sudo apt-get install -y arp-scan
+    else
+        echo "arp-scan ya está instalado."
+    fi
+}
+
+echo "Introduce un número: 
+1. Ejecutar
+2. Actualizar 
+3. Instalar Recursos"
+read -p "> " numero
+
+case $numero in
+    1)
+        ejecutar
+        ;;
+    2)
+        actualizar
+        ;;
+    3)
+        instalarRecursos
+        ;;
+    *)
+        echo "Número no válido. Por favor, introduce 1, 2 o 3."
+        ;;
+esac
